d909728822f4b64c805f4332a3b40f2ad69ce0a8
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-03-07 13:27:10 -0800

    Knock not doen

---
+++ src/extra/KnockKnock.java
@@ -0,0 +1,28 @@
+package extra;
+
+import javax.swing.JOptionPane;
+
+public class KnockKnock {
+	public static void main(String[] args) {
+
+
+
+JOptionPane.showMessageDialog(null,"knock knock");
+JOptionPane.inputDialog(null); 
+JOptionPane.showMessageDialog(null,"cow");
+
+
+
+
+
+
+
+
+
+
+
+
+
+}}
+
+
--- src/strings_and_dialogs/Morningzombie.java
+++ src/strings_and_dialogs/Morningzombie.java
@@ -12,11 +12,10 @@ public static void main(String[] args) {
 	// get up and go eat 
 	JOptionPane.showMessageDialog(null,"Good morning zombie go eat ");
 	// make them go brush there theet
-	JOptionPane.showMessageDialog(null,"kflskjdljhg");
-
-
-	
-JOptionPane.showMessageDialog(null,"");
+	JOptionPane.showMessageDialog(null,"zombie go brush you'er the");
+    // go take a shower 
+	JOptionPane.showMessageDialog(null,"zombie go take a shower");
+	// go get g
 
 }
 }
