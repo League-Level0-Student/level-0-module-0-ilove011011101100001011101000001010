b44dab11754c46a75acb0aa8653752c080ac9682
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-03-14 13:22:25 -0700

    do on thrus day

---
+++ src/Birthday.java
@@ -0,0 +1,53 @@
+
+public class Birthday {
+	public static void main(String[] args) {
+		
+		
+	// if your birthday is on 7/19/2005
+String hi =JOptionPaneInput.showInputDialog();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+}}
+
+
+
+
+
+
+
+
+
