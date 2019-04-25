66303367e2750303aa862571929ac4b7a8d0369e
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-04-25 13:15:36 -0700

    done with unbithday

--- src/Birthday.java
+++ src/Birthday.java
@@ -1,13 +1,21 @@
+import javax.swing.JOptionPane;
 
 public class Birthday {
 	public static void main(String[] args) {
 		
 		
 	// if your birthday is on 7/19/2005
-String hi =JOptionPaneInput.showInputDialog();
-		
-		
-		
+String hi =JOptionPane.showInputDialog(null, "hi is your birthday on 7/19/2005");
+//String No =JOptionPane.showInputDialog(null,"well then happy unbirthday");		
+if (hi.equals("yes")){
+	JOptionPane.showInputDialog("well then happy birthday");
+	//String No =JOptionPane.showInputDialog(null,"well then happy unbirthday");		
+
+}	
+else {	
+	String No =JOptionPane.showInputDialog(null,"well then happy unbirthday");		
+
+}		
 		
 		
 		
