619659efa1057f677b5a6ae00d7f48ad95490c0c
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-05-02 13:25:05 -0700

    Remarkble need to be done next week

---
+++ src/Remarkable.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class Remarkable {
+	public static void main(String[] args) {
+//carlosJesus,Melanie,Ayden,Jacob,Luis,Johnpaul,daniel,Hieu,diego  
+String ayden="is bad at fn and bad at apeg";		
+String jacob="is good at fn and good at apeg";
+String luis="is the beast at apeg and ok at fn";
+String hieu="idk i berlly evne KnOW THAtkid";
+String name=JOptionPane.showInputDialog(null,"what is your name");
+if (name.equals("ayden")){
+
+	
+}
+}}
